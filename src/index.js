fe0f74d76352706b76b0442b3a4db22eca162c35
Max Dombrowski <m.dombrowski@stroeermediabrands.de>
2019-05-10 22:41:34 +0200

    add prototype for desktop environment

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import './assets/demo.scss'
 
 const defaults = {
   dataSelector: '#galleryData',
-  stageSelector: '',
-  contentSelector: '',
+  stageSelector: '#galleryStage',
+  contentSelector: '#galleryContent',
 }
 
 let settings = {}
-let state = {
-  data: {},
-  currentPage: 0,
-  length: 0,
-}
+let state = {}
 
 /**
  * [init description]
@@ -25,6 +21,15 @@ const init = (options) => {
   state.data = getData()
   state.length = state.data.itemListElement.length
 
+  const hashPage = parseInt(location.hash.replace(/^\D+/g, ''))
+
+  if (typeof hashPage === 'number' && hashPage <= state.length && hashPage > 0) {
+    state.currentPage = hashPage
+  } else {
+    state.currentPage = 1
+    history.replaceState({}, '', '#page-1')
+  }
+
   renderPage()
 }
 
@@ -35,41 +40,46 @@ const getData = () => {
 }
 
 const renderPage = () => {
-  document.querySelector(settings.stageSelector).innerHTML = renderStage(state)
-  document.querySelector(settings.contentSelector).innerHTML = renderContent(state)
-
+  document.querySelector(settings.stageSelector).innerHTML = renderStage()
+  document.querySelector(settings.contentSelector).innerHTML = renderContent()
   bindEvents()
 }
 
 const renderStage = () => {
-  const page = state.data.itemListElement[state.currentPage]
+  const prevPage = state.data.itemListElement[state.currentPage - 2]
+  const page = state.data.itemListElement[state.currentPage - 1]
+  const nextPage = state.data.itemListElement[state.currentPage]
 
   return `
     <div class="smb-gallery-header">
-      <a role="smb-gallery-prev" class="smb-gallery-nav smb-gallery-nav-left" href="#">
+      ${state.currentPage > 1 ? `
+      <a role="smb-gallery-prev" class="smb-gallery-nav smb-gallery-nav-left" href="${prevPage.item.url}">
         <div class="smb-gallery-button">
           <i class="fas fa-angle-left"></i>
         </div>
       </a>
+      ` : ''}
       <div class="smb-gallery-media">
         ${page.item['@type'] === 'ImageObject' ? `<img class="" src="${page.item.contentUrl}" alt="">` : ''}
       </div>
-      <a role="smb-gallery-next" class="smb-gallery-nav smb-gallery-nav-right" href="#">
+      ${state.currentPage < state.length ? `
+      <a role="smb-gallery-next" class="smb-gallery-nav smb-gallery-nav-right" href="${nextPage.item.url}">
         <div class="smb-gallery-button">
           <i class="fas fa-angle-right"></i>
         </div>
       </a>
+      ` : ''}
     </div>
 
     <div class="smb-gallery-info">
-      <small>Bildquelle: Lorem Impsum</small>
-      <small>${state.currentPage + 1} / ${state.length}</small>
+      <small>Bildquelle: ${page.item.copyrightHolder}</small>
+      <small>${state.currentPage} / ${state.length}</small>
     </div>
   `
 }
 
 const renderContent = () => {
-  const page = state.data.itemListElement[state.currentPage]
+  const page = state.data.itemListElement[state.currentPage - 1]
 
   return `
     <div class="smg-gallery-body">
@@ -78,6 +88,34 @@ const renderContent = () => {
       <div class="smb-gallery-btn-nav">
         <a role="smb-gallery-prev" class="btn btn-link" href="#"><i class="fas fa-angle-left"></i> zurück zum Artikel</a>
         <a role="smb-gallery-next" class="btn btn-primary" href="#">weiter <i class="fas fa-angle-right"></i></a>
+      </div>
+
+      <div>
+        ${state.currentPage === state.length ? `
+          <h3>Weitere Bilderstrecken</h3>
+          <div class="row" style="margin-bottom: 25px">
+            <div class="col-md-4">
+              <div class="" style="height: 125px; background: #ddd;"></div>
+            </div>
+            <div class="col-md-4">
+              <div class="" style="height: 125px; background: #ddd;"></div>
+            </div>
+            <div class="col-md-4">
+              <div class="" style="height: 125px; background: #ddd;"></div>
+            </div>
+          </div>
+          <div class="row" style="margin-bottom: 25px">
+            <div class="col-md-4">
+              <div class="" style="height: 125px; background: #ddd;"></div>
+            </div>
+            <div class="col-md-4">
+              <div class="" style="height: 125px; background: #ddd;"></div>
+            </div>
+            <div class="col-md-4">
+              <div class="" style="height: 125px; background: #ddd;"></div>
+            </div>
+          </div>
+        ` : ''}
       </div>
     </div>
   `
@@ -111,24 +149,25 @@ const bindEvents = () => {
 }
 
 const goPrev = () => {
-  if (state.currentPage > 0) {
+  if (state.currentPage > 1) {
     state.currentPage = state.currentPage - 1
-    renderPage()
-
-    if (typeof settings.onItemChange === 'function') {
-      settings.onItemChange(state)
-    }
+    go()
   }
 }
 
 const goNext = () => {
-  if (state.currentPage < state.length - 1) {
+  if (state.currentPage < state.length) {
     state.currentPage = state.currentPage + 1
-    renderPage()
+    go()
+  }
+}
 
-    if (typeof settings.onItemChange === 'function') {
-      settings.onItemChange(state)
-    }
+const go = () => {
+  history.pushState({}, '', '#page-' + state.currentPage)
+  renderPage()
+
+  if (typeof settings.onItemChange === 'function') {
+    settings.onItemChange(state)
   }
 }
 
