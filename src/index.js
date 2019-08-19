b66f029202e0592c0691a294b38c5585e8220784
Max Dombrowski <m.dombrowski@stroeermediabrands.de>
2019-08-19 12:55:52 +0200

    Reverse hunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import './stylesheets/gallery.scss'
 import desktop from './desktop'
 import mobile from './mobile.js'
 
+if (process.env.NODE_ENV === 'development') {
+  import('./stylesheets/demo.scss')
+}
+
 const init = (options) => {
   if (options && options.device === 'smartphone') {
     mobile(options)
