fcbba77c5ebe84fabe39220d7dbd22120d0a2f09
Tungse <t.pham@stroeermediabrands.de>
2019-08-27 15:53:00 +0200

    Update gallery-data test with error handling

--- tests/unit/common/gallery-data.test.js
+++ tests/unit/common/gallery-data.test.js
@@ -1,21 +1,49 @@
 import getData from '../../../src/js/common/gallery-data.js'
 
 test('getData reads data from document json-ld', () => {
-  document.body.innerHTML = `
-  <script type="application/ld+json" id="data">
+  const testCases = [
     {
-      "@context": "http://schema.org",
-      "@type": "ItemList",
-      "numberOfItems": 4,
-      "itemListElement": []
-    }
-  </script>
-  `
+      innerHTML: `<script type="application/ld+json" id="data">  </script>`,
+      expected: () => {
+        expect(() => {
+          getData('#data')
+        }).toThrow('data is empty')
+      },
+    },
+    {
+      innerHTML: `<script type="application/ld+json" id="some-data">
+          {
+            "@context": "http://schema.org",
+          }
+        </script>`,
+      expected: () => {
+        expect(() => {
+          getData('#data')
+        }).toThrow('data element not found')
+      },
+    },
+    {
+      innerHTML: `<script type="application/ld+json" id="data">
+          {
+            "@context": "http://schema.org",
+            "@type": "ItemList",
+            "numberOfItems": 4,
+            "itemListElement": []
+          }
+        </script>`,
+      expected: () => {
+        expect(getData('#data')).toEqual({
+          '@context': 'http://schema.org',
+          '@type': 'ItemList',
+          'numberOfItems': 4,
+          'itemListElement': [],
+        })
+      },
+    },
+  ]
 
-  expect(getData('#data')).toEqual({
-    '@context': 'http://schema.org',
-    '@type': 'ItemList',
-    'numberOfItems': 4,
-    'itemListElement': [],
-  })
+  for (let testCase of testCases) {
+    document.body.innerHTML = testCase.innerHTML
+    testCase.expected()
+  }
 })
