7c319acb743d518c348fc6b76bf7ce3148691529
Tungse <t.pham@stroeermediabrands.de>
2020-03-29 13:31:25 +0200

    Dist

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = (env) => {
       rules: [
         {
           test: /\.js$/,
-          exclude: /node_modules\/.*/,
+          exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
           },
